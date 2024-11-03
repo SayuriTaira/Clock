885678e10242eaecbd18c129bcd452c98b159d50
SayuriTaira <142639287+SayuriTaira@users.noreply.github.com>
2024-11-03 11:10:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hours = document.getElementById('horas')
+const minutes = document.getElementById('minutos')
+const seconds = document.getElementById('segundos')
+
+setInterval(function time () {
+    let dateToday = new Date()
+    
+    let hr = dateToday.getHours()
+    let min = dateToday.getMinutes()
+    let sec = dateToday.getSeconds()
+
+    if (hr < 10) hr = '0' + hr
+    if (min < 10) min = '0' + min
+    if (sec < 10) sec = '0' + sec
+
+    hours.textContent = hr
+    minutes.textContent = min
+    seconds.textContent = sec
+})
